bd8c50b5bcfa3d736639ee6c67117981b15f98e5
Ken Doman <kdoman@bruceharris.com>
2014-09-23 11:28:41 -0500

    Field Tester rewritten in vanilla JavaScript. Warns if not valid map service layer url. No ArcGIS API required.

--- FieldTester.js
+++ FieldTester.js
@@ -1,80 +1,70 @@
 (function () {
-    var url = "http://js.arcgis.com/3.8/", scr1;
-    function loadMe() {
-        require(["dojo/ready", "dojo/dom-construct", "dojo/on", "esri/request", "esri/tasks/query", "esri/tasks/QueryTask"
-        ], function (ready, domConstruct, on, esriRequest, Query, QueryTask) {
-            var qt = new QueryTask(window.location.href),
-                queryMe = function (tbody, f, i) {
-                    var q = new Query(),
-                           timeCheck;
-                    if (i < f.length) {
-                        q.returnGeometry = false;
-                        q.outFields = [];
-                        q.where = ["NOT", f[i].name, "IS NULL"].join(" ");
-                        timeCheck = Date.now();
-                        qt.executeForCount(q).then(function (resp) {
-                            var tr = domConstruct.create("tr", {}, tbody),
-                                results = [f[i].name, f[i].alias, f[i].type, resp, Date.now() - timeCheck],
-                                x;
-                            for (x = 0; x < 5; x++) {
-                                domConstruct.create("td", {
-                                    innerHTML: results[x]
-                                }, tr);
-                            }
-                            queryMe(tbody, f, ++i);
-                        });
-                    }
-                };
-            ready(function () {
-                var div = domConstruct.create("div", {
-                    style: "position:fixed;bottom:5px;top:50%;right:5px;width:50%;border:1px solid #0F0;padding:5px;background:#fff;overflow:auto;text-align:center;"
-                }, document.body),
-
-                closer = domConstruct.create("button", {
-                    type: "button",
-                    innerHTML: "Close",
-                    style: "float: right;"
-                }, div);
-
-                on(closer, "click", function () {
-                    domConstruct.destroy(div);
-                });
-
-                esriRequest({
-                    url: qt.url,
-                    handleAs: "json",
-                    content: { f: "json" }
-                }).then(function (response) {
-                    if (response.fields) {
-                        var tbl = domConstruct.create("table", {
-                            style: "position:relative;margin:0 auto;"
-                        }, div),
-                            tbdy = domConstruct.create("tbody", {}, tbl);
-                        domConstruct.create("tr", {
-                            innerHTML: "<th>" + ["Field Name", "Alias", "Type", "Records with values", "Return Time (ms)"].join("</th><th>") + "</th>"
-                        }, tbdy);
-                        queryMe(tbdy, response.fields, 0);
-                    }
-                }, function (err) {
-                    alert(err.toString());
-                });
-            });
-        });
-    }
-    function scriptLoaded(url) {
-        var scripts = document.getElementsByTagName("script"), i;
-        for (i = scripts.length - 1; i > -1; i--) {
-            if (scripts[i].src === url) { return true; }
-        }
-        return false;
-    }
-    if (!scriptLoaded(url)) {
-        scr1 = document.createElement("script");
-        scr1.setAttribute("type", "text/javascript");
-        scr1.onload = loadMe;
-        scr1.setAttribute("src", url);
-        document.body.appendChild(scr1);
-    } else {
-        loadMe();
-    }
+    var url = window.location.href,
+		d = document,
+		node;
+	// ajax function from https://gist.github.com/Xeoncross/7663273
+	function ajax(u, callback, data, x) {
+		try {
+			x = new(this.XMLHttpRequest || ActiveXObject)("MSXML2.XMLHTTP.3.0");
+			x.open(data ? "POST" : "GET", u, 1);
+			x.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			x.onreadystatechange = function () {
+				x.readyState > 3 && callback && callback(x.responseText, x);
+			};
+			x.send(data)
+		} catch (e) {
+			window.console && console.log(e);
+		}
+	}
+	function queryMe (f) { 
+		if (!f.length) { return; }
+		var field = f.shift(),
+			params = "/query?where=not+field+is+null&returnGeometry=false&returnCountOnly=true&f=json".replace("field", field.name),
+			timeCheck = Date.now();
+		ajax(url + params,
+			function (response) {
+				response = JSON.parse(response);
+				var tr = d.createElement("tr");
+				tr.innerHTML = "<td>" + [field.name, field.alias, field.type, response.count, Date.now() - timeCheck].join("</td><td>") + "</td>";
+				node.appendChild(tr);
+				if (f.length) {	queryMe(f); }
+			});
+	}
+	function onLoad(response) {
+		response = JSON.parse(response);
+		if (response.fields) {
+			// create div
+			var div = d.createElement("div");
+			div.setAttribute("style", "position:fixed;bottom:5px;top:50%;right:5px;width:50%;border:1px solid #0F0;padding:5px;background:#fff;overflow:auto;text-align:center;");
+			d.body.appendChild(div);
+			// create close button
+			var closer = d.createElement("button");
+			closer.setAttribute("type", "button");
+			closer.setAttribute("style", "float:right;");
+			closer.onclick = function () { d.body.removeChild(div) };
+			closer.innerHTML = "close";
+			div.appendChild(closer);
+			// create results table
+			var table = d.createElement("table");
+			table.setAttribute("style", "position:relative;margin:0 auto;");
+			table.innerHTML = "<thead><tr><th>" + ["Field Name", "Alias", "Type", "# values not null", "Return Time (ms)"].join("</th><th>") + "</th></tr></thead>";
+			div.appendChild(table);
+			// add table body.
+			node = d.createElement("tbody");
+			table.appendChild(node);
+			// run query
+			queryMe(response.fields);
+		}
+	}
+    
+	if (/\d+\/?$/.test(url)) {
+		ajax(url + "?f=json", onLoad);
+	} else {
+		if (!/(map|feature)server\/?$/i.test(url)) {
+			alert("Pick a map/feature service and layer");
+		} else {
+			alert("Pick a valid layer");
+		}
+	}
 }());
