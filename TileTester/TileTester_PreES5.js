7c5ef1215eb62aa717b5a68332f398bcda51f7bb
Ken Doman <kdoman@bruceharris.com>
2016-01-12 14:57:02 -0600

    TileTester - Adding pre-ES5 code in separate line for older browsers (that won't go away).

---
+++ TileTester/TileTester_PreES5.js
@@ -0,0 +1,52 @@
+(function () {
+    var d = document,
+		tags = d.getElementsByTagName("a"),
+		urls = [],
+    i;
+  function insertAfter(newNode, refNode) {
+		refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
+	}
+	function Result(message, color) {
+		var item = d.createElement("span");
+		item.innerHTML = message;
+		item.setAttribute("style", "font-weight:bold;color:" + color + ";");
+		return item;
+	}
+	function onSuccess(index) {
+		insertAfter(Result(" OK ", "green"), tags[index]);
+		this.parentNode.removeChild(this);
+	}
+  function onError(index, error) {
+		insertAfter(Result(" !!! ", "red"), tags[index]);
+		console.error(error);
+		this.parentNode.removeChild(this);
+	}
+  function queryMe(f) {
+      if (!f.length) { return; }
+      var data = f.shift(),
+			  img = d.createElement("img");
+		img.onerror = function (err) { onError.call(this, data.i, err); };
+		img.onload = function () { onSuccess.call(this, data.i); };
+		img.setAttribute("src", data.url);
+		insertAfter(img, tags[data.i]);
+		window.setTimeout(function () {
+			queryMe(f);
+		}, 50);
+  }
+  if (/(map|feature)server\/?$/i.test(window.location.href)) {
+    for (i = 0; i < tags.length; i++) {
+      if (/server\/tile(\/\d+){3}\/?$/i.test(tags[i].href)) {
+        urls.push({ i: i, url: tags[i].href });
+      }
+    }
+	  if (urls && urls.length) {
+	    queryMe(urls);
+	  }
+
+  } else if (/server\/\d+\/?$/i.test(window.location.href)) {
+    alert("Go to the parent map service layer.");
+  } else {
+    alert("Pick a valid layer");
+  }
+
+}());
